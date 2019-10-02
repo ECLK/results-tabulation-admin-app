424513572d263693d305469c42011fa416c7cd80
Brion Mario <info@brionmario.com>
2019-10-02 12:04:19 +0530

    chore(configs): add endpoints config

--- src/configs/endpoints.ts
+++ src/configs/endpoints.ts
@@ -16,5 +16,18 @@
  * under the License.
  */
 
-export * from "./app";
-export * from "./endpoints";
+import { AppConfig } from "./app";
+
+const appConfig = new AppConfig();
+
+const IS_ENDPOINT = appConfig.ISEndpoint;
+
+export const RESOURCE_ENDPOINTS = {
+    authorize: `${IS_ENDPOINT}/oauth2/authorize`,
+    jwks: `${IS_ENDPOINT}/oauth2/jwks`,
+    logout: `${IS_ENDPOINT}/oidc/logout`,
+    users: `${IS_ENDPOINT}/scim2/Users`,
+    revoke: `${IS_ENDPOINT}/oauth2/revoke`,
+    token: `${IS_ENDPOINT}/oauth2/token`,
+    wellKnown: `${IS_ENDPOINT}/oauth2/oidcdiscovery/.well-known/openid-configuration`,
+};
