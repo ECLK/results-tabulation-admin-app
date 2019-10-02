ebbe9b730d235e8721de92d26f7ca3030549169e
Brion Mario <info@brionmario.com>
2019-10-02 12:03:49 +0530

    chore(configs): add missing config getters

--- src/configs/app.ts
+++ src/configs/app.ts
@@ -17,9 +17,15 @@
  */
 
 const DEFAULT_CONFIG = {
-    BASE_PATH: "/",
+    BASE_PATH: "admin",
+    HOME_PATH: "/overview",
+    LOGIN_PATH: "/login",
     IS_ENDPOINT: "https://localhost:9443",
-    TABULATION_API_ENDPOINT: "https://api.tabulation.ecstag.opensource.lk"
+    TABULATION_API_ENDPOINT: "https://api.tabulation.ecstag.opensource.lk",
+    CLIENT_ID: "ECLK_ADMIN_APP",
+    CLIENT_HOST: "https://localhost:9000",
+    LOGIN_CALLBACK_URL: "https://localhost:9000/login",
+    LOGOUT_CALLBACK_URL: "https://localhost:9000/logout"
 };
 
 /**
@@ -37,6 +43,28 @@ export class AppConfig {
         return process.env.REACT_APP_BASE_PATH
             ? process.env.REACT_APP_BASE_PATH
             : DEFAULT_CONFIG.BASE_PATH;
+    }
+
+    /**
+     * Returns the app home path.
+     *
+     * @return {string}
+     */
+    public get homePath(): string {
+        return process.env.REACT_APP_HOME_PATH
+            ? process.env.REACT_APP_HOME_PATH
+            : DEFAULT_CONFIG.HOME_PATH;
+    }
+
+    /**
+     * Returns the app login path.
+     *
+     * @return {string}
+     */
+    public get loginPath(): string {
+        return process.env.REACT_APP_LOGIN_PATH
+            ? process.env.REACT_APP_LOGIN_PATH
+            : DEFAULT_CONFIG.LOGIN_PATH;
     }
 
     /**
@@ -59,5 +87,49 @@ export class AppConfig {
         return process.env.REACT_APP_TABULATION_API_ENDPOINT
             ? process.env.REACT_APP_TABULATION_API_ENDPOINT
             : DEFAULT_CONFIG.TABULATION_API_ENDPOINT;
+    }
+
+    /**
+     * Returns the client id.
+     *
+     * @return {string}
+     */
+    public get clientID(): string {
+        return process.env.REACT_APP_CLIENT_ID
+            ? process.env.REACT_APP_CLIENT_ID
+            : DEFAULT_CONFIG.CLIENT_ID;
+    }
+
+    /**
+     * Returns the client host.
+     *
+     * @return {string}
+     */
+    public get clientHost(): string {
+        return process.env.REACT_APP_CLIENT_HOST
+            ? process.env.REACT_APP_CLIENT_HOST
+            : DEFAULT_CONFIG.CLIENT_HOST;
+    }
+
+    /**
+     * Returns the login callback url.
+     *
+     * @return {string}
+     */
+    public get loginCallbackURL(): string {
+        return process.env.REACT_APP_LOGIN_CALLBACK_URL
+            ? process.env.REACT_APP_LOGIN_CALLBACK_URL
+            : DEFAULT_CONFIG.LOGIN_CALLBACK_URL;
+    }
+
+    /**
+     * Returns the logout callback url.
+     *
+     * @return {string}
+     */
+    public get logoutCallbackURL(): string {
+        return process.env.REACT_APP_LOGOUT_CALLBACK_URL
+            ? process.env.REACT_APP_LOGOUT_CALLBACK_URL
+            : DEFAULT_CONFIG.LOGOUT_CALLBACK_URL;
     }
 }
