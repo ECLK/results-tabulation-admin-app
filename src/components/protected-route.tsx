d62405d45969d3ad0c2fee56d04ab37001a48590
Brion Mario <info@brionmario.com>
2019-10-02 12:02:06 +0530

    chore(components): implement a protected route component

---
+++ src/components/protected-route.tsx
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+import { history, updateAuthenticationCallbackUrl } from "../utils";
+import { useSelector } from "react-redux";
+import { AppState } from "../store";
+import { AppConfig } from "../configs";
+
+const appConfig = new AppConfig();
+
+export const ProtectedRoute = ({ component: Component, ...rest }) => {
+    const isAuth = useSelector((state: AppState) => state.authenticate.isAuth);
+
+    /**
+     * Update existing location path in the state to recall upon page refresh or authentication callback.
+     */
+    if (history.location.pathname !== appConfig.loginPath) {
+        updateAuthenticationCallbackUrl(history.location.pathname);
+    }
+
+    return (
+        <Route
+            render={(props) =>
+                isAuth ?
+                    <Component {...props} /> :
+                    <Redirect to={ appConfig.loginPath } />
+            }
+            {...rest}
+        />
+    );
+};
