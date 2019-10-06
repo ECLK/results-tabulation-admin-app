c5630673deda6f6cd7b25e40b29c7fbc7201c906
Brion Mario <info@brionmario.com>
2019-10-06 10:48:00 +0530

    chore(configs): add groups endpoint

--- src/configs/endpoints.ts
+++ src/configs/endpoints.ts
@@ -27,6 +27,7 @@ export const RESOURCE_ENDPOINTS = {
     jwks: `${IS_ENDPOINT}/oauth2/jwks`,
     logout: `${IS_ENDPOINT}/oidc/logout`,
     users: `${IS_ENDPOINT}/scim2/Users`,
+    groups: `${IS_ENDPOINT}/scim2/Groups`,
     revoke: `${IS_ENDPOINT}/oauth2/revoke`,
     token: `${IS_ENDPOINT}/oauth2/token`,
     wellKnown: `${IS_ENDPOINT}/oauth2/oidcdiscovery/.well-known/openid-configuration`,
