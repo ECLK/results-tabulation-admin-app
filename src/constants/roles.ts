c64857927bc3459f00e54fb01c111e513881cdcb
Brion Mario <info@brionmario.com>
2019-10-23 22:51:09 +0530

    chore(constants): add role display names to a constant

---
+++ src/constants/roles.ts
@@ -0,0 +1,10 @@
+export const ROLE_DISPLAY_NAMES = {
+    data_editor: "Data editor",
+    pol_div_rep_view: "Poling division report viewer",
+    pol_div_rep_verf: "Poling division report verifier",
+    elc_dis_rep_view: "Electoral district report viewer",
+    elc_dis_rep_verf: "Electoral district report verifier",
+    nat_dis_rep_view: "National report viewer",
+    nat_dis_rep_verf: "National report verifier",
+    ec_leadership: "EC leadership"
+};
